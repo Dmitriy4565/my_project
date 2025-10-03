7759e4f4f6b32f3a7d3c4e39542b186e1ff7d6f4
Dmitriy Prihodin <prihodin816@gmail.com>
2025-10-03 18:24:33 +0300

    Второе задние

--- Laba_3.py
+++ Laba_3.py
@@ -1,3 +1,7 @@
 # 1. List comprehension (простое преобразование)
 squares = [x**2 for x in range(1, 11)]
 print("1. Квадраты чисел от 1 до 10:", squares)
+
+# 2. List comprehension (фильтрация)
+even_numbers = [x for x in range(1, 20) if x % 2 == 0]
+print("2. Чётные числа от 1 до 20:", even_numbers)
